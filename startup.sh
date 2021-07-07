e5ea8b182a374bb40323a37734fe86aa0e28f2b4
AshiqMehmood <bolt7033@gmail.com>
2021-07-07 18:35:20 +0100

    updated auto script

--- startup.sh
+++ startup.sh
@@ -7,10 +7,13 @@ sudo service motion start
 sleep 1
 
 python3 /home/pi/Documents/baby_live/pyserver/indication.py
-
 sleep 1
 
 python3 /home/pi/message.py
 sleep 1
 
+cd /home/pi/Documents/baby_live
+git pull origin master
+echo "pulling latest update..."
+sleep 1
 python3 /home/pi/Documents/baby_live/pyserver/main.py
